6f703339db2aab14b894b79cfddab2a06f0a797f
vuraprasad <vuraprasad.cloud@gmail.com>
2026-03-10 21:10:38 +0530

    day10terraform-basic

---
+++ day1-terrform-basic/main.tf
@@ -0,0 +1,19 @@
+resource "aws_vpc" "my_vpc" {
+
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "my-vpc"
+  }
+
+}
+
+resource "aws_instance" "my_server" {
+
+  ami           = "ami-0abcdef12345"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "my-ec2-server"
+  }
+}
